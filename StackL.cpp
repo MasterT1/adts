6fa880fdc82416e2d1d14ed7ca2c705eac5ee641
MasterT1 <MasterT1@users.noreply.github.com>
2017-03-07 11:30:38 -0400

    Started on the stackl.cpp

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,30 @@
 #include "StackL.h"
+#include <iostream>
+#include <stdexcept>//used to be able to "throw" exceptions
+
+using namespace std;
+
+int Stack::size()
+ {
+    return data.size();
+ }
+
+void Stack::push(int val)
+{
+	data.insert(int val, int k);
+ }
+
+void Stack::pop()
+{
+	data.remove(int k);
+}
+	
+int Stack::top()
+{
+	data.get(int k);	
+}
+	
+void Stack::clear()
+{
+	data.clear();
+}
